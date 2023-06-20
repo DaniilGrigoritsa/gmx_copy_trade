c07cce855dae04d0d3a243d73e61519fc4832a82
daniilgrigoritsa <daniilgrigorica@gmail.com>
2023-06-20 23:42:41 +0300

    Updates

---
+++ static/js/180.0d42e6ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgmx_app=self.webpackChunkgmx_app||[]).push([[180],{69180:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=180.0d42e6ef.chunk.js.map
---
+++ static/js/942.dd7449d5.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkgmx_app=self.webpackChunkgmx_app||[]).push([[942],{24654:function(){},52361:function(){},94616:function(){}}]);
